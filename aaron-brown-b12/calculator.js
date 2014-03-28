e5ae48efb56eb27a7ba18f64cf9d7db68aabab31
Aaron Brown <aaron.kaoru.brown@gmail.com>
2014-03-27 22:13:36 -0700

    add comments to calculator.js file

--- aaron-brown-b12/calculator.js
+++ aaron-brown-b12/calculator.js
@@ -1,8 +1,9 @@
-//$('.calculator').on('click', 'button', function(){
+// global var for storing result for future computation
 var memoryStorage;
-
+// click on a button
 $('.calcButton').click(function(){
   var clicked = $(this);
+  // retrieve type of button pressed plus values of text fields
   var operation = clicked.data('operation');
   var num1 = +$('#numberField').val();
   var num2 = +$('#numberField2').val();
@@ -10,30 +11,42 @@ $('.calcButton').click(function(){
   //alert(num1);
   //alert(num2);
   var total;
+  // conditions for different math operations based on type of button pressed
+  // addition
   if(operation === "addition"){
     total = num1 + num2;
   } else if(operation === "subtraction"){
+    //subtraction
     total = num1 - num2;
   } else if(operation === "multiply"){
+    // multiplication
     total = num1 * num2;
   } else if(operation === "divide"){
+    // division
     total = num1 / num2;
   } else if(operation === "exponent"){
+    // exponential
     total = Math.pow(num1, num2);
   } else if(operation === "logarithm"){
+    // logarithm
     var logBase = 1 / num2;
     total = Math.pow(num1, logBase);
   } else if(operation == "clearAll"){
+    // clear all fields
     total = 0;
     $('#numberField').val("");
     $('#numberField2').val("");
   } else if(operation === "storeResult"){
+    // Get result and store in global var memoryStorage
     var totalToStore = $('#result').text();
     memoryStorage = totalToStore;
   } else if(operation === "recallResult"){
+    // Retrieve value of memoryStorage and fill in #numberField
     $('#numberField').val(memoryStorage);
   } else {
+    // Whoops
     alert("Ya done goofed");
   }
+  // Write final result to #result
   $('#result').text(total);
 });
